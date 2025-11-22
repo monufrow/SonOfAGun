35f68e0665f51818691d8efac018d50ce7752bb3
Mitch Onufrow <monufrow@trinity.edu>
2025-11-21 23:49:20 -0600

    New EnemyBase parent class, to make raycast hits cleaner

---
+++ Assets/Scripts/EnemyBase.cs
@@ -0,0 +1,31 @@
+using UnityEngine;
+using System.Collections;
+
+public class EnemyBase : MonoBehaviour
+{
+    public int health;
+
+    public virtual void TakeDamage(int amount)
+    {
+        health -= amount;
+        if (health <= 0) Destroy(gameObject);
+        HitEffect();
+    }
+    public virtual void HitEffect()
+    {
+        // Default hit effect (can be overridden)
+    }
+    public virtual IEnumerator HitEffectRoutine()
+    {
+        yield return null;
+    }
+    public virtual void Die()
+    {
+        Destroy(gameObject);
+    }
+    public virtual IEnumerator DieRoutine()
+    {
+        Destroy(gameObject);
+        yield return null;
+    }
+}
